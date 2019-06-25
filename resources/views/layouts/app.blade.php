1107767f27e016ddc41d0cdbca91b8fa813d2ff1
Edmund <45868739+EDstudent@users.noreply.github.com>
2019-06-25 13:59:55 +0300

    4 problems on 25.06

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,12 +39,14 @@
                             <li><a class="nav-link" href="/admin">@lang('messages.admin_panel')</a></li>
                         @endif
                       
+                      @if ( !Auth::user()->isUser() )
                         <li><a class="nav-link" href="/home">@lang('messages.home')</a></li>
                         <li><a class="nav-link" href="/offer">@lang('messages.offer')</a></li>
                         <li><a class="nav-link" href="/product">@lang('messages.product')</a></li>
                         <li><a class="nav-link" href="/search">@lang('messages.search')</a></li>
                         <li><a class="nav-link" href="/lang/en">ENG</a></li>
                         <li><a class="nav-link" href="/lang/lv">LV</a></li>
+                      @endif  
                     </ul> 
 
                     <!-- Right Side Of Navbar -->
